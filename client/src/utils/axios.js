981f9b1dabce37585ae4b72e543f60aca1692271
madas02d <madas02d@gmail.com>
2025-08-22 22:43:00 +0200

    e-commerce

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5002';
 
 const instance = axios.create({
-    baseURL,
+    baseURL,    
     withCredentials: true
 });
 
