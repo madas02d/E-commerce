277453a1c8c58d71b3f4191a29f205d230723874
madas02d <madas02d@gmail.com>
2025-08-27 12:45:07 +0200

    new update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   // base: '/e-commerce',
   server: {
     proxy: {
-      '/api': 'http://localhost:5173'
+      '/api': 'http://localhost:5173/products'
     },
     port: 5173,
     strictPort: false, // Allow fallback ports
